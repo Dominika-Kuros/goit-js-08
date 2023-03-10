35a8a461352423c3825a6071573e652ed70d6805
Dominika-Kuros <113472378+Dominika-Kuros@users.noreply.github.com>
2023-03-10 13:00:47 +0100

    2 commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+ const markup = createItemsMarkup(galleryItems);
+ gallery.insertAdjacentHTML("beforeend", markup);
+
+
+
+ function createItemsMarkup(item) {
+ return galleryItems
+   .map(({ preview, original, description }) => {
+     return `<div class="gallery__item">
+ <a class="gallery__link" href="${original}">
+ <img
+ class="gallery__image"
+ src="${preview}"
+ alt="${description}"/></a></div>`;})
+     .join("");
+  
+ }
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 50,
+});
