293866a357ad45e030816a23258e045081806030
Dominika-Kuros <113472378+Dominika-Kuros@users.noreply.github.com>
2023-03-10 13:56:31 +0000

    Deploying to gh-pages from @ Dominika-Kuros/goit-js-08@496f583f4eed0792c751c0fef07898a39b68eb0b 🚀

--- 02-video.f10ba389.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#vimeo-player"),t=new Player(e);t.on("timeupdate",throttle((()=>{localStorage.setItem("videoplayer-current-time",seconds)}),1e3)),t.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time"))).then((function(e){})).catch((function(e){e.name}));
-//# sourceMappingURL=02-video.f10ba389.js.map
