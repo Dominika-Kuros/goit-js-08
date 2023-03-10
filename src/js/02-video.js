3e93e7fc1e814aa31ce1d94e1e83d8426de36b14
Dominika-Kuros <113472378+Dominika-Kuros@users.noreply.github.com>
2023-03-10 13:04:21 +0100

    all files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
 
+
+const playerCurrentTime = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const pauseTime = () => {
+  localStorage.setItem(playerCurrentTime, seconds);
+};
+player.on('timeupdate', throttle(pauseTime, 1000));
+
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(playerCurrentTime)))
+.then(function(seconds) {})
+.catch (function(error) {
+    switch (error.name) {
+        case 'RangeError':
+           
+            break;
+
+        default:
+          
+            break;
+    }
+});
+
+
+// player.setCurrentTime(JSON.parse(localStorage.getItem(playerCurrentTime)) || 0);
