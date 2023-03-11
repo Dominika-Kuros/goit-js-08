33a6fac2b8dd2c6a451b49693542a27aeebae5e3
Dominika-Kuros <113472378+Dominika-Kuros@users.noreply.github.com>
2023-03-11 10:56:19 +0100

    updated video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,7 +8,7 @@ const player = new Player(iframe);
 // const pauseTime = (e) => {
 //   localStorage.setItem(playerCurrentTime, e.seconds);
 // };
-player.on('timeupdate', throttle(player.setCurrentTime(JSON.parse(localStorage.getItem(playerCurrentTime))), 1000));
+
 
 
 player.setCurrentTime(JSON.parse(localStorage.getItem(playerCurrentTime)))
@@ -26,3 +26,4 @@ player.setCurrentTime(JSON.parse(localStorage.getItem(playerCurrentTime)))
 });
 
 
+player.on('timeupdate', throttle(player.setCurrentTime, 1000));
